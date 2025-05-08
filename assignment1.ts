ca5d2d83d7fdeb01d20fad3779e51c66e56a2268
Dipto0000 <dipto2041@gmail.com>
2025-05-08 22:42:53 +0600

    two problems solved

--- assignment1.ts
+++ assignment1.ts
@@ -1,6 +1,13 @@
 {
+    //
     function formatString(input: string, toUpper: boolean = true): string {
-        return toUpper ? input.toUpperCase() : input.toLowerCase();
-      }
-      
+  return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+  }
+  
+
+//
 }
